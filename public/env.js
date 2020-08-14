26feca6ccdd73d964da8fdb65f9220813776b059
Katrina Steen <s.katrina@y7mail.com>
2020-08-14 14:46:36 +1000

    functional but messy web style

--- public/env.js
+++ public/env.js
@@ -1,4 +1,28 @@
 $(document).ready(function(){
-  console.log('Ready')
+  console.log('Ready');
 
+  //this block is used to post messages to the boar
+  $('#btnMessage').click(()=>{
+    let message=$('#messageBox').val();
+    let data = {
+      message
+    }
+    $.get('/message', data, function(){
+
+    })
+  })
+
+  //this block handles retrieval of messages, adds them to board
+
+  //get messages
+  setInterval(()=>{
+  $.get('/messages', function(messages){
+    $('#messages').empty();
+    console.log(messages);
+    messages.forEach((message)=>{
+console.log(message.message);
+$('#messages').append('<div class="row">'+message.message+'</div>');
+  })
+})
+  },1000)
 })
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
+const MongoClient = require('mongodb').MongoClient;
 var express = require("express"),
-    app = express();
+  app = express();
 
 var port = process.env.PORT || 8080;
 
@@ -9,6 +10,47 @@ app.get("/sayHello", function (request, response) {
   var user_name = request.query.user_name;
   response.end("Hello " + user_name + "!");
 });
+
+app.get('/messages', function (req, res) {
+  retrieveMessages(res);
+})
+
+app.get('/message', function (req, res) {
+  let message = req.query.message;
+  insertMessage(message);
+  res.send('added');
+})
+//DATABASE MANAGEMENT
+
+
+const uri = "mongodb+srv://sit725:sit725@sit725.63pic.mongodb.net/sitboard?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+let collectionMessage;
+
+
+
+client.connect(err => {
+  collectionMessage = client.db("sitboard").collection("messages");
+
+});
+
+//insert message
+const insertMessage = (message) => {
+  collectionMessage.insertOne({message: message })
+}
+
+const retrieveMessages = (res) => {
+  collectionMessage.find().toArray(function (err, result) {
+    if (err) throw err;
+    res.send(result);
+  })
+
+}
+
+setTimeout(function () {
+  insertMessage('Hello world')
+}, 1000);
 
 app.listen(port);
 console.log("Listening on port ", port);
